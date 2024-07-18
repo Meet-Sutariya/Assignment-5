fff5f6bdba01fab61d68b1c44d88d3cd3b8d601a
Meet-Sutariya <93037611+Meet-Sutariya@users.noreply.github.com>
2024-07-18 18:35:18 -0400

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -3,15 +3,21 @@ package main
 import "testing"
 
 func TestFindLargest(t *testing.T) {
-	result := FindLargest([]int{1, 2, 3, 4, 5})
-	expected := 5
+	result := FindLargest([]int{7, 12, 3, 45, 23})
+	expected := 45
 	if result != expected {
-		t.Errorf("FindLargest([]int{1, 2, 3, 4, 5}) = %d; want %d", result, expected)
+		t.Errorf("FindLargest([]int{7, 12, 3, 45, 23}) = %d; want %d", result, expected)
 	}
 
-	result = FindLargest([]int{-1, -2, -3, -4, -5})
-	expected = -1
+	result = FindLargest([]int{0, -5, 22, 11, -3})
+	expected = 22
 	if result != expected {
-		t.Errorf("FindLargest([]int{-1, -2, -3, -4, -5}) = %d; want %d", result, expected)
+		t.Errorf("FindLargest([]int{0, -5, 22, 11, -3}) = %d; want %d", result, expected)
+	}
+
+	result = FindLargest([]int{100, 250, 300, 200, 150})
+	expected = 300
+	if result != expected {
+		t.Errorf("FindLargest([]int{100, 250, 300, 200, 150}) = %d; want %d", result, expected)
 	}
 }
